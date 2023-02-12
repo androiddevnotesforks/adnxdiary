ca05e3edf513d2af93db12e0c16a5eb049b85d62
androiddevnotes <awesomedevnotes@gmail.com>
2023-02-12 11:47:02 +0000

    Visit androiddevnotes.com for detailed build instructions, a demonstration of the app, and additional information

---
+++ src/utils/ImageConfig.js
@@ -0,0 +1,8 @@
+export const imageIconSrc = 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/3.png';
+export const imageIconStyles = {
+  width: '100%',
+  height: 'auto',
+  maxWidth: '2em',
+  maxHeight: '2em',
+  objectFit: 'contain'
+};
---
+++ src/utils/UrlsConfig.js
@@ -0,0 +1,9 @@
+export const IMAGE_URLS = {
+  small: {
+    androidJetpackIcon: 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/3.png',
+    androidAppsIcon: 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/9.png',
+    androidInsightsIcon: 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/1.png',
+    androidToolsIcon: 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/6.png',
+    androidTutorialsIcon: 'https://www.androiddevnotes.com/api/raw/?path=/assets/1/7.png'
+  }
+};
